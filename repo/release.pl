600eb45099a06b679d7390030420c50b4429127b
philippe44 <philippe44@users.noreply.github.com>
2024-02-15 19:02:14 -0800

    Update release.pl

--- repo/release.pl
+++ repo/release.pl
@@ -6,12 +6,15 @@ use File::Basename;
 use Data::Dumper;
 use Digest::SHA1 qw(sha1 sha1_hex);
 
-my $url = $ARGV[2];
-my $zipfile = $ARGV[1];
+my $url = $ARGV[3];
+my $zipfile = $ARGV[2];
 my $repofile = $ARGV[0];
+my $version = $ARGV[1];
 
+=comment
 my $install = XMLin('install.xml', KeepRoot => 0, KeyAttr => '', NoAttr => 0);
 my $version = $install->{version};
+=cut
 print("$version\n");
 	
 my $repo = XMLin($repofile, ForceArray => 1, KeepRoot => 0, KeyAttr => 0, NoAttr => 0);
